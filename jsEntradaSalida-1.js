2f2c5f4735c0b44ac654dd1ba83dee400afe2e50
fulanodeta <avilaezequiel001@gmail.com>
2017-06-28 02:37:31 +0200

    CLASE 1

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,11 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+    var mensaje="esto funciona de maravilla"
+    alert(mensaje)
+    var nombre=prompt("ingresar")
+    alert(nombre)
+
+	
+}
+
